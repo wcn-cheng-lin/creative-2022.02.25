19b29810d959dc20b4daef7d7a65b6f0f27d248c
wcn-cheng-lin <100418781+wcn-cheng-lin@users.noreply.github.com>
2022-04-17 00:25:49 +0800

    exercise6

---
+++ script.js
@@ -0,0 +1,15 @@
+document.write("Hello, world!");
+function sayHello(){
+  var response=prompt("What's your name?");
+  alert("Hello "+response+" ,have a good day!");
+}
+function add(a,b){
+  return a + b;
+}
+console.log(add(20,30));
+console.log(add(25,500));
+function showtopic(){
+  var x =document.getElementById("demo");
+  x.style.fontsize="25px";
+  x.style.color="red";
+}
